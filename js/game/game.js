7812f5eefb7665c77be4e9489b6432ffab97290f
Jjilliann <jjilliann@lost-terminal.co.uk>
2014-12-13 15:06:22 +0000

    Use phaser group for bullets, remove bullets when out of bounds

--- js/game/game.js
+++ js/game/game.js
@@ -13,7 +13,14 @@ SkeletonWar.Game.prototype = {
 		this.player.speed = 200;
 		this.player.body.collideWorldBounds = true;
 
-		this.bullets = [];
+		this.bulletPool = this.add.group();
+		this.bulletPool.enableBody = true;
+		this.bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
+		this.bulletPool.createMultiple(100, 'bullet');
+		this.bulletPool.setAll('anchor.x', 0.5);
+		this.bulletPool.setAll('anchor.y', 0.5);
+		this.bulletPool.setAll('outOfBoundsKill', true);
+		this.bulletPool.setAll('checkWorldBounds', true);
 		this.nextShotAt = 0;
 		this.shotDelay = 100;
 
@@ -22,10 +29,6 @@ SkeletonWar.Game.prototype = {
 	update: function () {
 		dt = this.time.physicsElapsed;
 		this.bg.tilePosition.x -= this.player.speed * dt;
-
-		for (var i = 0; i < this.bullets.length; i++) {
-
-		}
 
 		this.player.body.velocity.x = 0;
 		this.player.body.velocity.y = 0;
@@ -56,13 +59,16 @@ SkeletonWar.Game.prototype = {
 		if (this.nextShotAt > this.time.now) {
 			return;
 		}
+
+		if (this.bulletPool.countDead() === 0) {
+			return;
+		}
+		
 		this.nextShotAt = this.time.now + this.shotDelay;
 
-		bullet = this.add.sprite(this.player.x + 20, this.player.y, 'bullet');
-		bullet.anchor.setTo(0.5, 0.5);
-		this.physics.enable(bullet, Phaser.Physics.ARCADE);
+		bullet = this.bulletPool.getFirstExists(false);
+		bullet.reset(this.player.x + 20, this.player.y);
 		bullet.body.velocity.x = 500;
-		this.bullets.push(bullet);
 	},
 	quitGame: function (pointer) {
 		this.state.start('MainMenu');
